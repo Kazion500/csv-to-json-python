43bad509da3561e6fc60fbdc9dfa266a447c316c
Patrick Kabwe <patrickckabwe@gmail.com>
2022-08-26 05:29:08 +0200

    chore: convert string points to float

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 
 import json
 
-def format_json(csv: str):
+def convert_string_points_to_float(points:list):
+    try:
+        points = [float(point) for point in points]
+    except ValueError:
+        points = [0,0]
+    return points
+
+def format_csv_to_json(csv: str):
     with open(csv, 'r') as f:
         points = f.read()
         points = points.split('\n')
         points = [point.split(',')[:2] for point in points]
         points = points[1:]
-        points = [point for point in points if point != ['']]
+        points = [convert_string_points_to_float(point) if point != [''] else [0,0] for point in points ]
 
         payload = {
             "geometry": {
@@ -16,11 +23,11 @@ def format_json(csv: str):
             "spatialReference": {
                 "wkid": 4326
             }
-        }
+        } 
         with open('payload.json', 'w') as f:
-            data = json.dumps(payload)
+            data = json.dumps(payload, indent=2)
             f.write(data)
 
 if __name__ == '__main__':
-    csv_file = "path_to_csv_file" # path to the csv file
-    format_json(csv_file)
+    csv_file = "Final.csv" # path to the csv file
+    format_csv_to_json(csv_file)
